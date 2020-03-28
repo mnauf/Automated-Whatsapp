ee207afcd0edcb76b8e2bb275be34e3cc12f610b
Muhammad Naufil <m.naufil1@gmail.com>
2020-03-28 15:49:13 +0500

    Fixed the bugs

--- speak_naufil.py
+++ speak_naufil.py
@@ -37,12 +37,12 @@ def eng():
         user.click()
 
     
-    msg_box=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME,'_1Plpp')))
+    msg_box=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME,'_2FBdJ')))
     for i in range(count):
         user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[title^='Attach']"))
         )
         user.click()
-        button=WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, 'GK4Lv'))) # Purple icon
+        button=WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, '_1azEi'))) # Purple icon
         driver.implicitly_wait(1)
         
         try:
@@ -57,52 +57,6 @@ def eng():
         time.sleep(4)
         time.sleep(sleeping)
 
-def ur():
-
-    name=input("Enter the name of person or group: ")
-    count=eval(input("How many times: "))
-    sleeping=eval(input("How many seconds delay?: "))
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    driver=webdriver.Chrome(dir_path+"\chromedriver.exe")
-
-    driver.get("https://web.whatsapp.com/")
-
-    xpath='//span[@title="{}"]'.format(name)
-    print(xpath)
-    user=WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH,xpath)))
-
-    try:
-        user.click()
-    except:
-        print ('StaleElementReferenceException while trying to type password, trying to find element again')
-        user=WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,xpath)))
-        user.click()
-
-    
-    msg_box=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME,'_1Plpp')))
-    for i in range(count):
-        user = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[title^='Attach']"))
-        )
-        user.click()
-        button=WebDriverWait(driver, 8).until(EC.element_to_be_clickable((By.CLASS_NAME, 'GK4Lv'))) # Purple icon
-        driver.implicitly_wait(1)
-        
-        try:
-            button.click()
-        except:
-            driver.implicitly_wait(1.5)
-            button.click()
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-
-        x=dir_path + r"\\urdu.exe"
-        os.startfile(x)
-        # os.startfile(r"D:\\Programming\\python\\whatsapp\\speech_voice\\urdu.exe")
-        button=WebDriverWait(driver, 12).until(EC.element_to_be_clickable((By.CLASS_NAME, '_3nfoJ'))) # send icon
-        button.click()
-        time.sleep(4)
-        time.sleep(sleeping)
-
 def text():
     name=input("Enter the name of person or group: ")
     msg=input("Enter your message: ")
@@ -111,13 +65,11 @@ def text():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     print(dir_path)
     driver=webdriver.Chrome(dir_path+"\chromedriver.exe")
-#     driver=webdriver.Chrome(r"C:\\setups\\chromedriver\\chromedriver.exe")
     driver.get("https://web.whatsapp.com/")
     xpath='//span[@title="{}"]'.format(name)
     print(xpath)
     user=WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH,xpath)))
-    # user=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,xpath)))
-    # user=driver.find_element_by_xpath('//span[@title="{}"]'.format(name))
+
         
     try:
         user.click()
@@ -125,8 +77,8 @@ def text():
         print ('StaleElementReferenceException while trying to type password, trying to find element again')
         user=WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,xpath)))
         user.click()
-    msg_box=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME,'_1Plpp')))
-    # msg_box=driver.find_element_by_class_name("_1Plpp")
+    msg_box=WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME,'_2FBdJ')))
+
     for i in range(count):
         msg_box.send_keys(msg)
         button=WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, '_35EW6')))
@@ -139,12 +91,10 @@ def main():
     print("Made by: Muhammad Naufil")
     print("Send your queries at m.naufil1@gmail.com")
     print()
-    choose=input("Choose English/Urdu/Text: ")
+    choose=input("Choose audio / Text: ")
     choose=choose.upper()
-    if choose=="ENGLISH":
+    if choose=="AUDIO":
         eng()
-    elif choose=="URDU":
-        ur()
     elif choose=="TEXT":
         text()
     else:
